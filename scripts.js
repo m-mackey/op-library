06e316509ff386a711f9f4a51de1020444e38743
mmackey <megan.mackey24@gmail.com>
2025-04-07 02:07:25 -0500

    Create Book constructor and fn to add books to library array

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author) {
   // the constructor...
+  if (!new.target) {
+    throw Error("Must use 'new' to call Book constructor");
+  }
+  this.title = title;
+  this.author = author;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author) {
   // take params, create a book then store it in the array
+  const bookToAdd = new Book(title, author);
+  myLibrary.push(bookToAdd);
 }
