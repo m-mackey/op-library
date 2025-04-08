ac66e612a991df7236586c033fc371bce7c4a281
mmackey <megan.mackey24@gmail.com>
2025-04-08 02:13:35 -0500

    Start function to display books
    wip, need to sync

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-const myLibrary = [];
+const myLibrary = [
+  {title: 'Gilgamesh', author: 'unknown'},
+];
 
 function Book(title, author) {
   // the constructor...
@@ -13,4 +15,12 @@ function addBookToLibrary(title, author) {
   // take params, create a book then store it in the array
   const bookToAdd = new Book(title, author);
   myLibrary.push(bookToAdd);
+}
+
+function displayBooks(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (const key in i) {
+      return arr[i][key];
+    }
+  }
 }
