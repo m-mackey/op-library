76822522392442554f8eaf82a2e48038c94b7800
mmackey <megan.mackey24@gmail.com>
2025-04-28 02:04:34 -0500

    Write most of function for removing books
    Figured out how to handle events with event delegation and making
    the parent tablet the thing with the event listener.
    
    Issue: a mysterious td is being added! I don't know how or why.
    Yet anyway. Will try to figure out later.

--- scripts.js
+++ scripts.js
@@ -22,9 +22,7 @@ function addBookToLibrary(title, author, pages, status) {
   displayNewBook(myLibrary.at(-1));
 }
 
-Book.prototype.toggleReadStatus = function () {
-  return 'worked'; 
-}
+
 
 const form = document.querySelector('form');
 const bookTitle = document.querySelector('#title');
@@ -66,8 +64,31 @@ function displayNewBook(book){ //will want to rename later probably. this or the
         <th scope="row">${book.title}</th>
         <td>${book.author}</td>
         <td>${book.pages}</td>
-        <td>${book.status}</td>
-        <td><button class="remove-btn">Remove</button><td>
+        <td><button class="read-status-btn">${book.status}</button></td>
+        <td><button class="remove-btn" data-book-id="${book.id}">Remove</button><td>
      </tr>`
   )
+}
+
+// Book.prototype.toggleReadStatus = function () {
+//   console.log('worked'); 
+// }
+
+libTableBody.addEventListener('click', removeBook); 
+libTableBody.addEventListener('click', toggleReadStatus); 
+
+function toggleReadStatus(e){  //event delegation? < add to 'what i learned'
+  if (e.target.classList.contains('read-status-btn')) { //might be better to target btn first then class?
+    console.log('read status button');
+    //call prototype fn here
+  } 
+}
+
+function removeBook(e) {
+  if(e.target.classList.contains('remove-btn')) {
+    //could add book id to variable so its a little easier to read
+    const findRow = document.querySelector(`tr[data-book-id='${e.target.dataset.bookId}']`);
+    findRow.remove();
+    //also need to remove from lib arr
+  }
 }
